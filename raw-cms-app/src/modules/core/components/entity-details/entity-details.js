5d3c538750d991d68cea821660afdfef0241bd42
Gabriele Raffaelli <g.raffaelli@sintraconsulting.eu>
2019-11-06 16:09:25 +0100

    no message

--- raw-cms-app/src/modules/core/components/entity-details/entity-details.js
+++ raw-cms-app/src/modules/core/components/entity-details/entity-details.js
@@ -1,13 +1,42 @@
+import { epicSpinners } from '../../../../utils/spinners.js';
+import { entitiesSchemaService } from '../../services/entities-schema.service.js';
+
 const _EntityDetails = async (res, rej) => {
   const tpl = await RawCMS.loadComponentTpl(
     '/modules/core/components/entity-details/entity-details.tpl.html'
   );
 
   res({
-    props: {
-      activeEntity: Object,
+    components: {
+      AtomSpinner: epicSpinners.AtomSpinner,
+    },
+    created: function() {
+      this.fetchEntityDetails();
+    },
+    data: () => {
+      return {
+        headers: [
+          { text: 'Name', value: 'Name' },
+          { text: 'Required', value: 'Required' },
+          { text: 'Type', value: 'Type' }
+        ],
+        isLoading: true,
+        fieldSettings: []
+      };
+    },
+    methods: {
+      fetchEntityDetails: function() {
+        setTimeout(async () => {
+          const res = await entitiesSchemaService.getEntityDetails(this.$route.params.id);
+          this.fieldSettings = res;
+          this.isLoading = false;
+        }, 1000);
+      }
     },
     template: tpl,
+    watch: {
+      $route: 'fetchData',
+    },
   });
 };
 
--- raw-cms-app/src/modules/core/services/entities-schema.service.js
+++ raw-cms-app/src/modules/core/services/entities-schema.service.js
@@ -12,6 +12,11 @@ class EntitiesSchemaService {
     const res = await this._apiClient.get('/system/admin/_schema');
     return res.data.data.items;
   }
+
+  async getEntityDetails(entityId) {
+    const res = await this._apiClient.get('/system/admin/_schema/' + entityId);
+    return res.data.data.FieldSettings;
+  }
 }
 
 export const entitiesSchemaService = new EntitiesSchemaService();
