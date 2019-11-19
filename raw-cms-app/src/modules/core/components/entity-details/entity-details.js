29f6d3ea711df49dcf60d1147e3163f940e4d47f
Daniele Fontani <zeppaman@altervista.org>
2019-11-19 18:47:57 +0100

    ui test

--- raw-cms-app/src/modules/core/components/entity-details/entity-details.js
+++ raw-cms-app/src/modules/core/components/entity-details/entity-details.js
@@ -1,5 +1,37 @@
 import { RawCmsDetailEditDef } from '../../../shared/components/detail-edit/detail-edit.js';
+import { RawCmsListDef } from '../../../shared/components/list/list.js';
 import { entitiesSchemaService } from '../../services/entities-schema.service.js';
+
+const _FieldListWrapperDef = async () => {
+  const rawCmsListDef = await RawCmsListDef();
+
+  return {
+    data: function() {
+      return {
+        apiService: entitiesSchemaService,
+      };
+    },
+    extends: rawCmsListDef,
+    methods: {
+      amdRequire: require,
+      deleteConfirmMsg(item) {
+        return this.$t('core.entities.deleteConfirmMsgTpl', { name: item.CollectionName });
+      },
+      deleteSuccessMsg(item) {
+        return this.$t('core.entities.deleteSuccessMsgTpl', { name: item.CollectionName });
+      },
+      deleteErrorMsg(item) {
+        return this.$t('core.entities.deleteErrorMsgTpl', { name: item.CollectionName });
+      },
+    },
+    props: {
+      detailRouteName: {
+        typ: String,
+        default: 'field-list',
+      },
+    },
+  };
+};
 
 const _EntityDetailsWrapperDef = async () => {
   const rawCmsDetailEditDef = await RawCmsDetailEditDef();
@@ -16,6 +48,7 @@ const _EntityDetailsWrapperDef = async () => {
 
 const _EntityDetailsDef = async () => {
   const detailWrapperDef = await _EntityDetailsWrapperDef();
+  const listWrapperDef = await _FieldListWrapperDef();
   const tpl = await RawCMS.loadComponentTpl(
     '/modules/core/components/entity-details/entity-details.tpl.html'
   );
@@ -23,8 +56,15 @@ const _EntityDetailsDef = async () => {
   return {
     components: {
       DetailWrapper: detailWrapperDef,
+      ListWrapper: listWrapperDef,
     },
-    props: detailWrapperDef.extends.props,
+    props: Vue.extend(detailWrapperDef.extends.props, {
+      monacoScriptOptions: {
+        language: 'javascript',
+        scrollBeyondLastLine: false,
+      },
+    }),
+    methods: detailWrapperDef.extends.methods,
     template: tpl,
   };
 };
